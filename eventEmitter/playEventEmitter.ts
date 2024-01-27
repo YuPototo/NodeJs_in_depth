7fd6f16cc94bf9ae01b75071489884b5fb4eb27a
Yu Qin <sirius.qinyu@gmail.com>
2024-01-27 11:25:16 +0900

    Event Emitter

--- eventEmitter/playEventEmitter.ts
+++ eventEmitter/playEventEmitter.ts
@@ -4,21 +4,31 @@ import EventEmitter from './MyEventEmitter'
 const eventEmitter = new EventEmitter()
 
 // --- on ---
-// eventEmitter.on('goal', () => {
-//     console.log('GOAL!')
-// })
-
-// eventEmitter.on('goal', () => {
-//     console.log('Nice Goal!')
-// })
-
-// eventEmitter.emit('goal')
-// eventEmitter.emit('goal')
-
-// --- once ---
-eventEmitter.once('yellowCard', () => {
-    console.log('Yellow Card!')
+eventEmitter.on('goal', () => {
+    console.log('GOAL!')
 })
 
+eventEmitter.on('goal', () => {
+    console.log('Nice Goal!')
+})
+
+eventEmitter.emit('goal')
+eventEmitter.emit('goal')
+
+// --- removeListener ---
+const giveYellowCard = () => {
+    console.log('Yellow Card!')
+}
+
+eventEmitter.on('yellowCard', giveYellowCard)
 eventEmitter.emit('yellowCard')
-eventEmitter.emit('yellowCard')
+eventEmitter.removeListener('yellowCard', giveYellowCard)
+eventEmitter.emit('yellowCard') // nothing happens
+
+// --- once ---
+eventEmitter.once('redCard', () => {
+    console.log('Red Card!')
+})
+
+eventEmitter.emit('redCard')
+eventEmitter.emit('redCard') // nothing happens
